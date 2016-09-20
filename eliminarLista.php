8bea7637f202617705bf631ddc6b27e0896d7b6d
Sergy <sergy4444@gmail.com>
2016-09-20 07:32:34 +0000

    datubasea konpondu nahian

--- eliminarLista.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dp = mysql_connect("localhost", "root", "zubiri" );
-mysql_select_db("todoList", $dp);
-$ezabatu = "DELETE FROM listas WHERE numLista='$_GET[numLista]'";
-mysql_query($ezabatu);
-mysql_close($dp);
-header("location:hasiera.html");
-?>
